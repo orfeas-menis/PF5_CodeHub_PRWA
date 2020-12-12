9ef1f116ade21e9bb84ac61ebcac88e34da17fb8
tzwnk <p3100080@aueb.gr>
2020-12-12 20:16:56 +0200

    Showing The Owners

--- src/main/java/pf5/codehub/team5/webapp/controller/OwnerController.java
+++ src/main/java/pf5/codehub/team5/webapp/controller/OwnerController.java
@@ -1,14 +1,26 @@
 package pf5.codehub.team5.webapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pf5.codehub.team5.webapp.model.UserModel;
+import pf5.codehub.team5.webapp.service.UserServiceImpl;
+
+import java.util.List;
 
 @Controller
 public class OwnerController {
+
+    private static final String USERS_LIST = "users";
+
+    @Autowired
+    private UserServiceImpl userService;
+
     @GetMapping(path = "/owner")
     public String adminHome(Model model) {
-
+        List<UserModel> users = userService.findAll();
+        model.addAttribute(USERS_LIST,users);
         return "owner";
     }
 }
