12ec3d4cebb8d3899304998aa687709d622508b6
tzwnk <p3100080@aueb.gr>
2020-12-14 16:11:54 +0200

    Owner Controller

--- src/main/java/pf5/codehub/team5/webapp/controller/OwnerController.java
+++ src/main/java/pf5/codehub/team5/webapp/controller/OwnerController.java
@@ -3,10 +3,12 @@ package pf5.codehub.team5.webapp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import pf5.codehub.team5.webapp.form.SearchForm;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import pf5.codehub.team5.webapp.model.UserModel;
 import pf5.codehub.team5.webapp.service.UserServiceImpl;
+
 import java.util.List;
 import java.util.Optional;
 
